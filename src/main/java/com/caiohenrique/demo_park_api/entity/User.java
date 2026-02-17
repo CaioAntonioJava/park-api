19008ba38c6733f7daa53afa758a2ad9def061d6
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-02-17 11:35:36 -0300

    feat(user): add User entity

---
+++ src/main/java/com/caiohenrique/demo_park_api/entity/User.java
@@ -0,0 +1,68 @@
+package com.caiohenrique.demo_park_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ïd")
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+
+    @Column(name = "created_at")
+    private LocalDateTime createAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updateAt;
+    @Column(name = "created_by")
+    private LocalDateTime createdBy;
+    @Column(name = "updated_by")
+    private LocalDateTime updatedBy;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
