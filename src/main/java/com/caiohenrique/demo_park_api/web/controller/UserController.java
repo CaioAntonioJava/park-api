c15d95c87c071bde07cc90d195d84aa2d49deca2
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-02-20 15:41:46 -0300

    docs(user): add Swagger documentation for create user endpoint

--- src/main/java/com/caiohenrique/demo_park_api/web/controller/UserController.java
+++ src/main/java/com/caiohenrique/demo_park_api/web/controller/UserController.java
@@ -6,6 +6,12 @@ import com.caiohenrique.demo_park_api.web.dto.UserCreateDTO;
 import com.caiohenrique.demo_park_api.web.dto.UserChangePasswordDTO;
 import com.caiohenrique.demo_park_api.web.dto.UserResponseDTO;
 import com.caiohenrique.demo_park_api.web.dto.mapper.UserMapper;
+import com.caiohenrique.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Usuários", description = "Contém todos as operaçõe relativas aos recursos para cadastro, edição e leitura de um usuário")
 @RestController
 @RequestMapping("api/v1/users")
 @RequiredArgsConstructor
@@ -21,6 +28,17 @@ public class UserController {
     private final UserService userService;
 
 
+    @Operation(
+            summary = "Criar um novo usuário", description = "Recurso para criar um novo usuário",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso", content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDTO.class))),
+                    @ApiResponse(responseCode = "409", description = "Usuário email já cadastrado no sistema", content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos", content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PostMapping
     public ResponseEntity<UserResponseDTO> create (@Valid @RequestBody UserCreateDTO createDto) {
         User user = userService.save(UserMapper.toUser(createDto));
