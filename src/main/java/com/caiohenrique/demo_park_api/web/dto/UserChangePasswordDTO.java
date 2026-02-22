e811d4a98cddcceae00b0f43616e1b13a2c3c5e6
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-02-22 10:49:05 -0300

    docs(user-dto): add @Schema examples to password fields

--- src/main/java/com/caiohenrique/demo_park_api/web/dto/UserChangePasswordDTO.java
+++ src/main/java/com/caiohenrique/demo_park_api/web/dto/UserChangePasswordDTO.java
@@ -1,5 +1,6 @@
 package com.caiohenrique.demo_park_api.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -13,13 +14,16 @@ public class UserChangePasswordDTO {
 
     @NotBlank
     @Size( min = 6, max = 6)
+    @Schema(example = "123456")
     private String currentPassword;
 
     @NotBlank
     @Size( min = 6, max = 6)
+    @Schema(example = "NovaSenha@123")
     private String newPassword;
 
     @NotBlank
     @Size( min = 6, max = 6)
+    @Schema(example = "NovaSenha@123")
     private String confirmPassword;
 }
