f0cbdc7ce072692cf50999efd41d0f3e1e7c0ba1
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-03-13 18:00:41 -0300

    refactor: define json property order for client projection

--- src/main/java/com/caiohenrique/demo_park_api/repository/projection/ClientProjection.java
+++ src/main/java/com/caiohenrique/demo_park_api/repository/projection/ClientProjection.java
@@ -1,5 +1,8 @@
 package com.caiohenrique.demo_park_api.repository.projection;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "name", "cpf"})
 public interface ClientProjection {
 
     Long getId();
