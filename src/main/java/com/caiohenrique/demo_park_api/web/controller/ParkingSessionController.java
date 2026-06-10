74d8f142ac44350e7428f676efa8e21f655e37ea
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-06-10 09:54:43 -0300

    docs: add OpenAPI documentation for check-in endpoint

--- src/main/java/com/caiohenrique/demo_park_api/web/controller/ParkingSessionController.java
+++ src/main/java/com/caiohenrique/demo_park_api/web/controller/ParkingSessionController.java
@@ -5,8 +5,16 @@ import com.caiohenrique.demo_park_api.service.ParkingLotService;
 import com.caiohenrique.demo_park_api.web.dto.ParkingSessionCreateDTO;
 import com.caiohenrique.demo_park_api.web.dto.ParkingSessionResponseDTO;
 import com.caiohenrique.demo_park_api.web.dto.mapper.ParkingSessionMapper;
+import com.caiohenrique.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +32,29 @@ public class ParkingSessionController {
 
     private final ParkingLotService parkingLotService;
 
+
+    @Operation(
+            summary = "Operação de check-in", description = "Recurso para dar entrada em um veículo no estacionamento. " +
+            "Requisição exige uso de um bearer token. Acesso restrito para ADMIN.",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso.",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL de acesso ao recurso criado"),
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ParkingSessionResponseDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Causas possíveis: <br/>" +
+                            "- CPF do cliente não cadastrado no sistema <br/>" +
+                            "- Nenhuma vaga livre foi localizada",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de CLIENTE",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+            }
+    )
     @PostMapping("check-in")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ParkingSessionResponseDTO> checkIn(@RequestBody @Valid ParkingSessionCreateDTO createDTO) {
