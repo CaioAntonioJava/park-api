f8938ede1348161bc8fceea9f492ed5da5a61e35
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-06-13 12:20:53 -0300

    docs: update example email in UserLoginDTO

--- src/main/java/com/caiohenrique/demo_park_api/web/dto/UserLoginDto.java
+++ src/main/java/com/caiohenrique/demo_park_api/web/dto/UserLoginDto.java
@@ -13,7 +13,7 @@ import lombok.*;
 @ToString
 public class UserLoginDTO {
 
-    @Schema(description = "Email do usuário utilizado para login", example = "usuario@email.com")
+    @Schema(description = "Email do usuário utilizado para login", example = "usuario@email.com.br")
     @NotBlank
     @Email(message = "Email inválido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
