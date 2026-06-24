ac3eb13d442c4744f1e388d0b180b6c056a900f4
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-06-24 15:28:57 -0300

    refactor: standardize ResponseEntity status creation

--- src/main/java/com/caiohenrique/demo_park_api/web/controller/ClientController.java
+++ src/main/java/com/caiohenrique/demo_park_api/web/controller/ClientController.java
@@ -22,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -69,7 +70,7 @@ public class ClientController {
 
         clientService.save(client);
 
-        return ResponseEntity.status(201).body(ClientMapper.clientResponseDTO(client));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ClientMapper.clientResponseDTO(client));
     }
 
     @Operation(
