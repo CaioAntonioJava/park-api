711cbd7417da1a79579361d59d88c9e29f30a1a3
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-06-24 15:16:24 -0300

    refactor: standardize repository annotations

--- src/main/java/com/caiohenrique/demo_park_api/repository/ParkingSpotRepository.java
+++ src/main/java/com/caiohenrique/demo_park_api/repository/ParkingSpotRepository.java
@@ -3,9 +3,11 @@ package com.caiohenrique.demo_park_api.repository;
 import com.caiohenrique.demo_park_api.entity.ParkingSpot;
 import com.caiohenrique.demo_park_api.enums.SpotStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
     Optional<ParkingSpot> findBySpotCode(String spotCode);
 
