fa85a8f5fe2717863f58425f30b3461952bc1e6d
CaioAntonioJava <caioantonio.dev@gmail.com>
2026-06-25 11:57:23 -0300

    refactor(repository): add pessimistic lock and timeout to critical queries

--- src/main/java/com/caiohenrique/demo_park_api/repository/ParkingSpotRepository.java
+++ src/main/java/com/caiohenrique/demo_park_api/repository/ParkingSpotRepository.java
@@ -2,7 +2,11 @@ package com.caiohenrique.demo_park_api.repository;
 
 import com.caiohenrique.demo_park_api.entity.ParkingSpot;
 import com.caiohenrique.demo_park_api.enums.SpotStatus;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +15,7 @@ import java.util.Optional;
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
     Optional<ParkingSpot> findBySpotCode(String spotCode);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
     Optional<ParkingSpot> findFirstBySpotStatusOrderBySpotCodeAsc(SpotStatus spotStatus);
 }
